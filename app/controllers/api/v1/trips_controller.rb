3dd63b6e38fbade8289ab3e4ab6204804b9489ff
skj1991 <stacykjohns@icloud.com>
2020-09-23 22:51:31 -0400

    tripscontroller: created index and create actions. set staus based in instance being saved. added strong params

---
+++ app/controllers/api/v1/trips_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::TripsController < ApplicationController
+    def index
+        trip = Trip.all 
+        render json: trip
+    end
+
+    def create
+        trip = Trip.new(trip_params)
+        if trip.save
+            render json: trip, status: :accepted
+        else
+            render json: {errors: trip.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def trip_params
+        params.require(:trip).permit(:title, :destination, :description, :rating, :must_visit, :fave_restaurant, :hotel, :image_url, :country_id)
+    end
+end
